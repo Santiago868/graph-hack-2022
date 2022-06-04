d4131b434092cd97468d0bbff62d32cd347e3b1c
CSW <santiagowilson@icloud.com>
2022-06-04 11:01:01 -0700

    Add react router

---
+++ my-app/src/pages/Results/index.tsx
@@ -0,0 +1,20 @@
+import { Heading } from '@chakra-ui/react'
+
+
+export const ResultsPage = () => {
+    return (
+        <div>
+            <div>
+                <Heading>
+                    INSERT TAGLINE
+                </Heading>
+            </div>
+           
+            
+            <div>
+
+            </div>
+        </div>
+    )
+};
+
