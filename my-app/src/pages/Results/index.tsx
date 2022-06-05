209dcb8ee30acef61f7252899f41656c25d9716c
CSW <santiagowilson@icloud.com>
2022-06-04 19:43:34 -0700

    chart setup

--- my-app/src/pages/Results/index.tsx
+++ my-app/src/pages/Results/index.tsx
@@ -1,19 +1,78 @@
-import { Heading } from '@chakra-ui/react'
+import { Center, Heading, Container } from '@chakra-ui/react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
 
 const Results = () => {
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+    
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Dstl VS Others',
+            },
+        },
+    };
+
+    const labels = ['PNG', 'JPEG', 'SVG']
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Others',
+                data: [ 6,
+                    12,
+                    4],
+                backgroundColor: 'rgba(128,128,128)',
+            },
+            {
+                label: 'DSTL',
+                data: [ 1,
+                    2,
+                    .5],
+                backgroundColor: 'rgba(51,255,51)',
+            },
+        ]
+    }
+
     return (
-        <div>
-            <div>
+        <>
+            <Center>
                 <Heading>
                     INSERT TAGLINE
                 </Heading>
-            </div>
-           
+            </Center>
+        
             
-            <div>
-
-            </div>
-        </div>
+            <Container
+                maxW='container.md' 
+            >
+                <Bar
+                    options={options}
+                    data={data}
+                />
+            </Container>
+        </>
     )
 };
 
