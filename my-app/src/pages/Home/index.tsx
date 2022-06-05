6ae4fa84850c86b698f1086ea041f3eaaffc1b03
wariroja <warinya.rojanasuwan@zumper.com>
2022-06-04 23:36:15 -0700

    full page

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -14,6 +14,10 @@ import FileUpload from '../FileUpload';
 import theme from '../../theme';
 import { useUploadImageMutation } from '../../redux/uploadAPI';
 import './style.css'
+import diagram from '../../../src/assets/diagram.png';
+import histogram from '../../../src/assets/histogram.png';
+
+import auto from '../../../src/assets/auto.png';
 
 const imageUploadSchema = object({
   imageCover: z.instanceof(File),
@@ -54,9 +58,45 @@ function Home() {
             alignItems: 'center',
             height: '100vh',
           }}
-        >x
-          <Box display='flex' flexDirection='column' sx={{ width: '50' }}>
+        >
+          <Box display='flex' flexDirection='column' sx={{ width: '50%' }}>
             {/* Single Image Upload */}
+            <Typography
+                  className="textLarge"
+                  textAlign='center'
+                  variant='h1'
+                  component='h1'
+                  gutterBottom
+                  fontFamily='Poppins'
+                  sx={{
+                    fontWeight: 500
+                  }}
+                >
+                  DSTL
+                </Typography>
+            <FormProvider {...methods}>
+              <Box
+                component='form'
+                noValidate
+                autoComplete='off'
+                onSubmit={methods.handleSubmit(onSubmitHandler)}
+              >
+              </Box>
+            </FormProvider>
+          </Box>
+        </Box>
+      </Container>
+      <CssBaseline />
+      <Container maxWidth={false} className='background'>
+        <Box
+          display='flex'
+          flexDirection='column'
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
             <Typography
                   className="text"
                   textAlign='left'
@@ -65,8 +105,20 @@ function Home() {
                   gutterBottom
                   fontFamily='Poppins'
                 >
-                  Multiple Image Upload
-                </Typography>
+                  Faster Better S<span className="text">a</span>
+            </Typography>
+            <Typography
+                  className="holder2"
+                  textAlign='left'
+                  fontFamily='Poppins'
+                  sx={{
+                    marginBottom: '60px'
+                  }}
+                >
+                  Because bigger is not always better
+            </Typography>
+          <Box display='flex' flexDirection='column' sx={{ width: '30%' }}>
+            {/* Single Image Upload */}
             <FormProvider {...methods}>
               <Box
                 component='form'
@@ -89,6 +141,101 @@ function Home() {
           </Box>
         </Box>
       </Container>
+      <Container maxWidth={false} className='background' style={{paddingTop: '50px', paddingBottom: '50px'}}>
+        <Box
+          display='flex'
+          flexDirection='column'
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+              <img
+                src={diagram}
+                alt='file upload' 
+              /> 
+        </Box>
+          
+      </Container>
+      <Container maxWidth={false} className='background' style={{paddingTop: '50px', paddingBottom: '50px'}}>
+      <Typography
+                  className="text"
+                  textAlign='center'
+                  variant='h2'
+                  component='h1'
+                  gutterBottom
+                  fontFamily='Poppins'
+                >
+                  Image Compression Results
+            </Typography>
+        <Box
+          display='flex'
+          flexDirection='column'
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+              <img
+                src={histogram}
+                alt='file upload' 
+              /> 
+        </Box>
+          
+      </Container>
+      <Container maxWidth={false} className='background' style={{paddingTop: '10px', paddingBottom: '20px'}}>
+        <Box
+          display='flex'
+          flexDirection='column'
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+              <img
+                src={auto}
+                alt='file upload' 
+              /> 
+        </Box>
+          
+      </Container>
+      <Container maxWidth={false} className='background'>
+        <Box
+          display='flex'
+          flexDirection='column'
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50vh',
+          }}
+        >
+          <Box display='flex' flexDirection='column' sx={{ width: '30%' }}>
+            {/* Single Image Upload */}
+            <FormProvider {...methods}>
+              <Box
+                component='form'
+                noValidate
+                autoComplete='off'
+                onSubmit={methods.handleSubmit(onSubmitHandler)}
+              >
+                <Button
+                  variant='contained'
+                  type='submit'
+                  fullWidth
+                  sx={{ py: '0.8rem', my: 2 }}
+                  href="https://mumbai.polygonscan.com/address/0x177F8A18cEf0cdDdaa7F5BB28a0FBcB38FA4699c"
+                >
+                  MINT NOW
+                </Button>
+              </Box>
+            </FormProvider>
+          </Box>
+        </Box>
+      </Container>
+      
     </ThemeProvider>
   );
 }
